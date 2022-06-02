30dca4e2060fedf94d45094fbaca06d7ca63b544
Soun9922 <sagsounak39@gmail.com>
2022-06-02 19:53:13 +0530

    The generics code have been Added

--- src/javagenerics/TestMaximum.java
+++ src/javagenerics/TestMaximum.java
@@ -23,16 +23,17 @@ public class TestMaximum<TM extends Comparable<TM>> {
         if (thirdValue.compareTo(maxValue) > 0) {
             maxValue = thirdValue;
         }
+        printMaximumValue(firstValue, secondValue, thirdValue, maxValue);
         return maxValue;
     }
 
     public static <TM> void printMaximumValue(TM firstValue, TM secondValue, TM thirdValue, TM maxValue) {
-        System.out.printf("Maximum Value is", firstValue, secondValue, thirdValue, maxValue);
+        System.out.printf("Maximum Value of %s , %s and %s is %s", firstValue, secondValue, thirdValue, maxValue);
     }
 
     public static void main(String[] args) {
         Integer fistValueInt = 6, secondValueInt = 9, thirdValueInt = 2;
 
-        new TestMaximum(fistValueInt, secondValueInt, thirdValueInt)
+        new TestMaximum(fistValueInt, secondValueInt, thirdValueInt).maximum();
     }
 }
